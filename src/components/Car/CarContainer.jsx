a895aa79be8eaccc3319ca0a42f5c387a78bdbd4
ibukunoluwanap <promisenap2001@gmail.com>
2022-10-21 18:06:52 +0300

    added footer component

--- src/components/Car/CarContainer.jsx
+++ src/components/Car/CarContainer.jsx
@@ -4,8 +4,58 @@ import { Container, Row, Col } from "react-bootstrap";
 import CarCard from './CarCard/CarCard';
 
 const CarContainer = ({ carSection }) => (
-    <Container ref={carSection} className="mt-5">
+    <Container ref={carSection} className="mt-5 mb-5">
         <Row>
+            <Col sm={3} className="mt-5">
+                <CarCard
+                    image="https://images.carexpert.com.au/app/uploads/2022/06/2023-bmw-ix1-xdrive30-HERO.jpg"
+                    name="Car name"
+                    brand="Car brand name"
+                    initialPrice={20.03}
+                    currentBidding={30.49}
+                    details="Sed ut eros ut velit iaculis facilisis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec malesuada magna ac ante fermentum, eu tempor metus convallis. Pellentesque malesuada urna sit amet luctus feugiat. Vivamus imperdiet nisi laoreet, pellentesque purus sit amet, elementum augue. Nam enim tellus, maximus quis turpis eget, molestie efficitur augue."
+                    balance={100.0} />
+            </Col>
+            <Col sm={3} className="mt-5">
+                <CarCard
+                    image="https://images.carexpert.com.au/app/uploads/2022/06/2023-bmw-ix1-xdrive30-HERO.jpg"
+                    name="Car name"
+                    brand="Car brand name"
+                    initialPrice={20.03}
+                    currentBidding={30.49}
+                    details="Sed ut eros ut velit iaculis facilisis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec malesuada magna ac ante fermentum, eu tempor metus convallis. Pellentesque malesuada urna sit amet luctus feugiat. Vivamus imperdiet nisi laoreet, pellentesque purus sit amet, elementum augue. Nam enim tellus, maximus quis turpis eget, molestie efficitur augue."
+                    balance={100.0} />
+            </Col>
+            <Col sm={3} className="mt-5">
+                <CarCard
+                    image="https://images.carexpert.com.au/app/uploads/2022/06/2023-bmw-ix1-xdrive30-HERO.jpg"
+                    name="Car name"
+                    brand="Car brand name"
+                    initialPrice={20.03}
+                    currentBidding={30.49}
+                    details="Sed ut eros ut velit iaculis facilisis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec malesuada magna ac ante fermentum, eu tempor metus convallis. Pellentesque malesuada urna sit amet luctus feugiat. Vivamus imperdiet nisi laoreet, pellentesque purus sit amet, elementum augue. Nam enim tellus, maximus quis turpis eget, molestie efficitur augue."
+                    balance={100.0} />
+            </Col>
+            <Col sm={3} className="mt-5">
+                <CarCard
+                    image="https://images.carexpert.com.au/app/uploads/2022/06/2023-bmw-ix1-xdrive30-HERO.jpg"
+                    name="Car name"
+                    brand="Car brand name"
+                    initialPrice={20.03}
+                    currentBidding={30.49}
+                    details="Sed ut eros ut velit iaculis facilisis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec malesuada magna ac ante fermentum, eu tempor metus convallis. Pellentesque malesuada urna sit amet luctus feugiat. Vivamus imperdiet nisi laoreet, pellentesque purus sit amet, elementum augue. Nam enim tellus, maximus quis turpis eget, molestie efficitur augue."
+                    balance={100.0} />
+            </Col>
+            <Col sm={3} className="mt-5">
+                <CarCard
+                    image="https://images.carexpert.com.au/app/uploads/2022/06/2023-bmw-ix1-xdrive30-HERO.jpg"
+                    name="Car name"
+                    brand="Car brand name"
+                    initialPrice={20.03}
+                    currentBidding={30.49}
+                    details="Sed ut eros ut velit iaculis facilisis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec malesuada magna ac ante fermentum, eu tempor metus convallis. Pellentesque malesuada urna sit amet luctus feugiat. Vivamus imperdiet nisi laoreet, pellentesque purus sit amet, elementum augue. Nam enim tellus, maximus quis turpis eget, molestie efficitur augue."
+                    balance={100.0} />
+            </Col>
             <Col sm={3} className="mt-5">
                 <CarCard
                     image="https://images.carexpert.com.au/app/uploads/2022/06/2023-bmw-ix1-xdrive30-HERO.jpg"
