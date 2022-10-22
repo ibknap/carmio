b3481953739c6f8ffc0b9a6408de7986b660e502
ibukunoluwanap <promisenap2001@gmail.com>
2022-10-22 22:47:29 +0300

    modified login navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from "react"
 import PropTypes from 'prop-types';
-import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { Button, Container, Nav, Navbar, OverlayTrigger, Tooltip, Alert } from "react-bootstrap";
 import './Navbar.css';
 
 function CusNavbar({ logo, balance, address, avatar }) {
     const [isAuth, setAuth] = useState(false);
 
     const loginInfo = <div className="cus-navbar-btn-avatar border-none border-1 cus-primary" variant="">
-        <div style={{width: "40px", background: "#000"}}>
+        <div style={{ width: "40px", background: "#000" }}>
             <img src={avatar} alt="avatar" className="avatar" />
         </div>
-        <div style={{marginLeft: "10px"}}>
+        <div style={{ marginLeft: "10px" }}>
             <b>{balance} ALGO</b>
             <br />
             addr: {address}
@@ -18,6 +18,22 @@ function CusNavbar({ logo, balance, address, avatar }) {
     </div>
 
     const loginBtn = <Button onClick={() => setAuth(!isAuth)} className="cus-navbar-btn border-none border-1 cus-primary" variant="">Login</Button>
+
+    const logoutBtn = <Nav.Link href="#" onClick={() => setAuth(false)} className="text-white">Logout</Nav.Link>
+
+    const createBtn = <OverlayTrigger
+        key="createBtn"
+        placement="bottom"
+        overlay={
+            <Tooltip id="createBtnTooltip">
+                <Alert variant="warning" className="p-2 border-none">
+                    Only Admin can create car post. Please contact admins!
+                </Alert>
+            </Tooltip>
+        }
+    >
+        <Nav.Link href="#" className="text-muted">Create</Nav.Link>
+    </OverlayTrigger>
 
     return (
         <Navbar bg="" expand="lg" fixed="top" className="cus-navbar">
@@ -27,10 +43,9 @@ function CusNavbar({ logo, balance, address, avatar }) {
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
-                    <Nav
-                        className="me-auto my-2 my-lg-0"
-                        navbarScroll
-                    >
+                    <Nav className="me-auto my-2 my-lg-0" navbarScroll>
+                        {isAuth && createBtn}
+                        {isAuth && logoutBtn}
                     </Nav>
                     {isAuth ? loginInfo : loginBtn}
                 </Navbar.Collapse>
