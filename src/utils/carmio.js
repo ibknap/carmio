955dcaf79f2259275cfc58e055e828ee3a4ce68a
ibukunoluwanap <promisenap2001@gmail.com>
2022-10-23 17:42:55 +0300

    add carmio.js

--- src/utils/carmio.js
+++ src/utils/carmio.js
@@ -93,3 +93,191 @@ export const createCarAction = async (senderAddress, car) => {
     console.log("Created new app-id: ", appId);
     return appId;
 }
+
+
+// BUY CAR AT INITIAL PRICING:
+export const buyCarAction = async (senderAddress, car) => {
+    console.log("Buying car...");
+
+    let params = await algodClient.getTransactionParams().do();
+
+    // Build required app args as Uint8Array
+    let buyArg = new TextEncoder().encode("buy")
+    let appArgs = [buyArg]
+
+    // Create ApplicationCallTxn
+    let appCallTxn = algosdk.makeApplicationCallTxnFromObject({
+        from: senderAddress,
+        appIndex: car.appId,
+        onComplete: algosdk.OnApplicationComplete.NoOpOC,
+        suggestedParams: params,
+        appArgs: appArgs
+    })
+
+    // Create PaymentTxn
+    let paymentTxn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+        from: senderAddress,
+        to: car.owner,
+        amount: car.initialPrice,
+        suggestedParams: params
+    })
+
+    let txnArray = [appCallTxn, paymentTxn]
+
+    // Create group transaction out of previously build transactions
+    let groupID = algosdk.computeGroupID(txnArray)
+    for (let i = 0; i < 2; i++) txnArray[i].group = groupID;
+
+    // Sign & submit the group transaction
+    let signedTxn = await myAlgoConnect.signTransaction(txnArray.map(txn => txn.toByte()));
+    console.log("Signed group transaction");
+    let tx = await algodClient.sendRawTransaction(signedTxn.map(txn => txn.blob)).do();
+
+    // Wait for group transaction to be confirmed
+    let confirmedTxn = await algosdk.waitForConfirmation(algodClient, tx.txId, 4);
+
+    // Notify about completion
+    console.log("Group transaction " + tx.txId + " confirmed in round " + confirmedTxn["confirmed-round"]);
+}
+
+
+// BID CAR PRICING:
+export const bidCarAction = async (senderAddress, car, bidPrice) => {
+    console.log("Biding car...");
+
+    let params = await algodClient.getTransactionParams().do();
+    bidPrice = stringToMicroAlgos(bidPrice)
+
+    // Build required app args as Uint8Array
+    let bidArg = new TextEncoder().encode("bid")
+    let appArgs = [bidArg]
+
+    // Create ApplicationCallTxn
+    let appCallTxn = algosdk.makeApplicationCallTxnFromObject({
+        from: senderAddress,
+        appIndex: car.appId,
+        onComplete: algosdk.OnApplicationComplete.NoOpOC,
+        suggestedParams: params,
+        appArgs: appArgs
+    })
+
+    // Create PaymentTxn
+    let paymentTxn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+        from: senderAddress,
+        to: car.owner,
+        amount: bidPrice,
+        suggestedParams: params
+    })
+
+    let txnArray = [appCallTxn, paymentTxn]
+
+    // Create group transaction out of previously build transactions
+    let groupID = algosdk.computeGroupID(txnArray)
+    for (let i = 0; i < 2; i++) txnArray[i].group = groupID;
+
+    // Sign & submit the group transaction
+    let signedTxn = await myAlgoConnect.signTransaction(txnArray.map(txn => txn.toByte()));
+    console.log("Signed group transaction");
+    let tx = await algodClient.sendRawTransaction(signedTxn.map(txn => txn.blob)).do();
+
+    // Wait for group transaction to be confirmed
+    let confirmedTxn = await algosdk.waitForConfirmation(algodClient, tx.txId, 4);
+
+    // Notify about completion
+    console.log("Group transaction " + tx.txId + " confirmed in round " + confirmedTxn["confirmed-round"]);
+}
+
+
+// GET CAR INFO:
+export const getCarsAction = async () => {
+    console.log("Fetching cars...")
+    let note = new TextEncoder().encode(carmioNote);
+    let encodedNote = Buffer.from(note).toString("base64");
+
+    // Step 1: Get all transactions by notePrefix (+ minRound filter for performance)
+    let transactionInfo = await indexerClient.searchForTransactions()
+        .notePrefix(encodedNote)
+        .txType("appl")
+        .minRound(minRound)
+        .do();
+
+    let cars = []
+
+    for (const transaction of transactionInfo.transactions) {
+        let appId = transaction["created-application-index"]
+        if (appId) {
+            // Step 2: Get each application by application id
+            let car = await getApplication(appId)
+            if (car) {
+                cars.push(car)
+            }
+        }
+    }
+    console.log("cars fetched.")
+    return cars;
+}
+
+
+// GET APPLICATION INFO:
+const getApplication = async (appId) => {
+    try {
+        // 1. Get application by appId
+        let response = await indexerClient.lookupApplications(appId).includeAll(true).do();
+        if (response.application.deleted) {
+            return null;
+        }
+        let globalState = response.application.params["global-state"]
+
+        // 2. Parse fields of response and return car
+        let owner = response.application.params.creator
+        let image = ""
+        let name = ""
+        let brand = ""
+        let initialPrice = 0
+        let currentBidding = 0
+        let description = ""
+        let sold = 0
+
+        const getField = (fieldName, globalState) => {
+            return globalState.find(state => {
+                return state.key === utf8ToBase64String(fieldName);
+            })
+        }
+
+        if (getField("IMAGE", globalState) !== undefined) {
+            let field = getField("IMAGE", globalState).value.bytes
+            image = base64ToUTF8String(field)
+        }
+
+        if (getField("NAME", globalState) !== undefined) {
+            let field = getField("NAME", globalState).value.bytes
+            name = base64ToUTF8String(field)
+        }
+
+        if (getField("BRAND", globalState) !== undefined) {
+            let field = getField("BRAND", globalState).value.bytes
+            brand = base64ToUTF8String(field)
+        }
+
+        if (getField("INITIAL PRICE", globalState) !== undefined) {
+            initialPrice = getField("INITIAL PRICE", globalState).value.uint
+        }
+
+        if (getField("CURRENT BIDDING", globalState) !== undefined) {
+            currentBidding = getField("CURRENT BIDDING", globalState).value.uint
+        }
+
+        if (getField("DESCRIPTION", globalState) !== undefined) {
+            let field = getField("DESCRIPTION", globalState).value.bytes
+            description = base64ToUTF8String(field)
+        }
+
+        if (getField("SOLD", globalState) !== undefined) {
+            sold = getField("SOLD", globalState).value.uint
+        }
+
+        return new Carmio(image, name, brand, initialPrice, currentBidding, description, sold, appId, owner)
+    } catch (err) {
+        return null;
+    }
+}
