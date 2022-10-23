380d9edd2f0885d793e6b9cf6ce793bead349d09
ibukunoluwanap <promisenap2001@gmail.com>
2022-10-23 16:48:16 +0300

    add algo constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -18,12 +18,12 @@ export const myAlgoConnect = new MyAlgoConnect();
 
 export const minRound = 21540981;
 
-// https://github.com/algorandfoundation/ARCs/blob/main/ARCs/arc-0002.md
-export const marketplaceNote = "tutorial-marketplace:uv1"
+export const marketplaceNote = "carmio-app:uv1"
 
 // Maximum local storage allocation, immutable
 export const numLocalInts = 0;
 export const numLocalBytes = 0;
+
 // Maximum global storage allocation, immutable
 export const numGlobalInts = 2; // Global variables stored as Int: count, sold
 export const numGlobalBytes = 3; // Global variables stored as Bytes: name, description, image
