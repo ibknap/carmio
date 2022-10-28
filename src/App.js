c93311e2d8c5adc49a4f3ad9e5beab3c28fc6ce0
ibukunoluwanap <promisenap2001@gmail.com>
2022-10-28 17:33:25 +0300

    min updates

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React, { useState, useRef } from "react";
 import Header from "./components/Header/Header";
 import headerImg from "./assets/header_img.png";
 import CusNavbar from "./components/Navbar/Navbar";
-import logo from "./assets/logo.png";
-import CarContainer from "./components/Car/CarContainer";
+import CarContainer from "./components/Car/CarContainer/CarContainer";
 import Footer from './components/Footer/Footer';
-import './App.css';
 import { indexerClient, myAlgoConnect } from "./utils/constants";
 import { NotificationError } from "./components/Notifications";
 import {toast} from "react-toastify";
+import logo from "./assets/logo.png";
+import './App.css';
 
 function App() {
+  // hoofs
   const carSection = useRef(null);
   const [address, setAddress] = useState(null);
   const [balance, setBalance] = useState(0);
+  // variables
   const avatar = "https://library.kissclipart.com/20191116/wze/kissclipart-avatar-icon-awesome-set-icon-forum-user-icon-18f64889026582d3.png";
   const goToCarContainer = () => carSection.current?.scrollIntoView({ behavior: 'smooth' });
 
